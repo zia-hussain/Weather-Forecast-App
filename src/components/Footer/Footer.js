e97ab664bdace9d3268277fed9a7c367a8b933e0
zia-hussain <syedziashahgillani76@gmail.com>
2024-02-19 12:46:26 +0500

    footer added and some styling !

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <footer className="pt-5 text-gray-700 dark:text-white font-semibold ">
+      <span>Open Source by </span>
+      <a
+        className="text-sky-600 hover:text-sky-500 dark:text-sky-400 dark:hover:text-sky-300"
+        href="https://myportfolio-zia.netlify.app"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Zia Hussain
+      </a>
+      <span> © 2024 - Now</span>
+    </footer>
+  );
+}
+
+export default Footer;

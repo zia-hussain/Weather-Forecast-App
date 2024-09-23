ef4f24783bc4b6920efa5d0b547eae4fa1b2edc9
zia-hussain <syedziashahgillani76@gmail.com>
2024-09-23 20:28:39 +0500

    Deplying ...

--- src/components/Main/ForecastDayItem.js
+++ src/components/Main/ForecastDayItem.js
@@ -2,10 +2,6 @@ import React, { useEffect } from "react";
 
 function ForecastDayItem({ temp, icon, day, hour }) {
   const weatherIcon = require(`../../icons/${icon}.svg`);
-  useEffect(() => {
-    console.log(weatherIcon);
-    console.log("icon", icon);
-  }, [icon]);
 
   return (
     <li className="flex flex-col justify-center items-center p-5 bg-white dark:bg-gray-900 rounded-xl py-2">

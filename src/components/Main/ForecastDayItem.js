cdcf2cede4b0e7d061db6683decc687d2ddc8294
zia-hussain <syedziashahgillani76@gmail.com>
2024-09-23 20:29:02 +0500

    Deplying ...

--- src/components/Main/ForecastDayItem.js
+++ src/components/Main/ForecastDayItem.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React from "react";
 
 function ForecastDayItem({ temp, icon, day, hour }) {
   const weatherIcon = require(`../../icons/${icon}.svg`);
